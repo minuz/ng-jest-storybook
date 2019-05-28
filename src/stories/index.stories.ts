2a121db12e0215c3dda27111441eaf6eef295199
Fernando Baba <fernando.baba@iress.com.au>
2019-05-28 16:14:19 +1000

    Added Storybook

--- src/stories/index.stories.ts
+++ src/stories/index.stories.ts
@@ -12,48 +12,22 @@ storiesOf('Welcome', module).add('to Storybook', () => ({
   props: {}
 }));
 
-storiesOf('Button', module)
-  .add('with text', () => ({
-    component: Button,
-    props: {
-      text: 'Hello Button'
-    }
-  }))
-  .add(
-    'with some emoji',
-    () => ({
-      component: Button,
-      props: {
-        text: '😀 😎 👍 💯'
-      }
-    }),
-    { notes: 'My notes on a button with emojis' }
-  )
-  .add(
-    'with some emoji and action',
-    () => ({
-      component: Button,
-      props: {
-        text: '😀 😎 👍 💯',
-        onClick: action('This was clicked OMG')
-      }
-    }),
-    { notes: 'My notes on a button with emojis' }
-  );
-
-storiesOf('Another Button', module).add(
-  'button with link to another story',
-  () => ({
-    component: Button,
-    props: {
-      text: 'Go to Welcome Story',
-      onClick: linkTo('Welcome')
-    }
-  })
-);
-
 storiesOf('Hello Lib', module)
-  .addDecorator(withTests({ results }))
-  .add('Simple Hello', () => ({ component: HelloComponent, props: {} }), {
-    jest: ['hello.component']
-  });
+  .addDecorator(
+    withTests({ results, filesExt: '((\\.specs?)|(\\.tests?))?(\\.ts)?$' })
+  )
+  .addParameters({ jest: ['HelloComponent'] }) // Doesn't work either.
+  // .add('Simple Hello', () => ({ component: HelloComponent, props: {} }), {
+  //   jest: ['hello.component']
+  // }); // Doesn't work as well
+  .add(
+    'Simple Hello',
+    () => ({
+      moduleMetadata: {
+        declarations: [HelloComponent]
+      },
+      template: `<lib-hello></lib-hello>`,
+      props: {}
+    }),
+    { jest: ['HelloComponent'] }
+  );
